8b32a4e23dc82824ed2fee0f486f39e3afa704d3
konstantin <nkp.yalta@mail.ru>
2022-06-24 14:44:41 +0300

    add exes1

---
+++ exes1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] CreatAndFillArr(int large, int min, int max)
+{
+    int[] array = new int[large];
+    for (int ifill = 0; ifill < array.Length; ifill++)
+    {
+        array[ifill] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+void printarr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+int SearchEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"Количество четных масивов = {count}");
+    return count;
+}
+int[] myArray = CreatAndFillArr(10, 100, 1000);
+printarr(myArray);
+SearchEvenNumbers(myArray);
